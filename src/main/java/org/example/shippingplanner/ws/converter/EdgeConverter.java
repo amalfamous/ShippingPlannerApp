2e6657d633c74a094512dbe2746342d85be4cc2a
amalfamous <amalfms@gmail.com>
2025-05-02 21:38:52 +0100

    node + edge converter

--- src/main/java/org/example/shippingplanner/ws/converter/EdgeConverter.java
+++ src/main/java/org/example/shippingplanner/ws/converter/EdgeConverter.java
@@ -1,4 +1,28 @@
 package org.example.shippingplanner.ws.converter;
 
+import org.example.shippingplanner.bean.Edge;
+import org.example.shippingplanner.ws.dto.EdgeDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class EdgeConverter {
+    public Edge toBean(EdgeDto dto){
+        Edge bean=new Edge();
+        BeanUtils.copyProperties(dto,bean);
+        return bean;
+    }
+    public List<Edge> toBean(List<EdgeDto> dtos){
+        return dtos.stream().map(this::toBean).toList();
+    }
+    public EdgeDto toDto(Edge bean){
+        EdgeDto dto=new EdgeDto();
+        BeanUtils.copyProperties(bean,dto);
+        return dto;
+    }
+    public List<EdgeDto> toDto(List<Edge> beans){
+        return beans.stream().map(this::toDto).toList();
+    }
 }
--- src/main/java/org/example/shippingplanner/ws/converter/NodeConverter.java
+++ src/main/java/org/example/shippingplanner/ws/converter/NodeConverter.java
@@ -1,4 +1,28 @@
 package org.example.shippingplanner.ws.converter;
 
+import org.example.shippingplanner.bean.Node;
+import org.example.shippingplanner.ws.dto.NodeDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class NodeConverter {
+    public Node toBean(NodeDto dto){
+        Node bean=new Node();
+        BeanUtils.copyProperties(dto,bean);
+        return bean;
+    }
+    public List<Node> toBean(List<NodeDto> dtos){
+        return dtos.stream().map(this::toBean).toList();
+    }
+    public NodeDto toDto(Node bean){
+        NodeDto dto=new NodeDto();
+        BeanUtils.copyProperties(bean,dto);
+        return dto;
+    }
+    public List<NodeDto> toDto(List<Node> beans){
+        return beans.stream().map(this::toDto).toList();
+    }
 }
