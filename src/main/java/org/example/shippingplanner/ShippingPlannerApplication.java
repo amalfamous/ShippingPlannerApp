fafb21c857433f8549ae34dcdb105ea9f48a918a
amalfamous <amalfms@gmail.com>
2025-05-02 19:16:06 +0100

    pom.xml complete

---
+++ src/main/java/org/example/shippingplanner/ShippingPlannerApplication.java
@@ -0,0 +1,13 @@
+package org.example.shippingplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShippingPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/shippingplanner/ShippingPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shippingplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
